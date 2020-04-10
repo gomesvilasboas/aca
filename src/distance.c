26dfdc8661a04899953b5bbc7186dd7ad93df992
Fabricio Vilasboas <gomesvilasboas@gmail.com>
2020-04-10 19:12:32 -0300

    Removida identificação de blocos.

--- src/distance.c
+++ src/distance.c
@@ -4,10 +4,10 @@ double manhattan_distance(double *item_value1, double *item_value2, const int n)
 {
   int i;
   double sum = 0.0;
+
   for(i = 0; i < n; ++i)
-  {
     sum += fabsf(item_value1[i]-item_value2[i]);
-  }
+
   return sum/(double)n;
 }
 
@@ -17,8 +17,7 @@ double euclidian_distance(double *item_value1, double *item_value2, const int n)
   double sum = 0.0;
 
   for (i = 0; i < n; i++)
-  {
     sum += pow(item_value1[i] - item_value2[i], 2);
-  }
+
   return sqrt(sum/(double)n);
 }
